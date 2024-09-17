178af14b1e4ff42c197e378e9947ecd922320973
Mehmet Deniz Kumcu <68655812+devmdeniz@users.noreply.github.com>
2024-09-17 19:32:32 +0300

    Update update-readme.js

--- update-readme.js
+++ update-readme.js
@@ -1,10 +1,8 @@
 const fs = require('fs');
 
-// Dosyaları oku
-const projectsFile = fs.readFileSync('Projects.md', 'utf8');
+const projectsFile = fs.readFileSync('projects.md', 'utf8');
 const readmeFile = fs.readFileSync('README.md', 'utf8');
 
-// #tables başlığındaki kısmı ayıkla
 const tablesSectionRegex = /<!--START_SECTION:ProjectsList-->([\s\S]*?)<!--END_SECTION:ProjectsList-->/i;
 const projectsTableMatch = projectsFile.match(tablesSectionRegex);
 if (!projectsTableMatch) {
@@ -14,15 +12,14 @@ if (!projectsTableMatch) {
 
 const projectsTable = projectsTableMatch[1];
 
-
 const rows = projectsTable
   .trim()
   .split('\n')
   .slice(2)
   .map(row => row.split('|').map(cell => cell.trim()))
-  .filter(row => row.length > 1 && row[3] !== '')
-  .sort((a, b) => parseInt(b[3]) - parseInt(a[3]))
-  .slice(0, 5);
+  .filter(row => row.length > 1 && row[4] !== '')
+  .sort((a, b) => parseInt(b[4], 10) - parseInt(a[4], 10)) 
+  .slice(0, 5); 
 
 
 const updatedTable = ['| Project name | Description | Main Language or Technology | Commit Number | STATE | STATUS |', 
