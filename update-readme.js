23e46e0f5c071668b70d71e5170b956dec8fadda
Mehmet Deniz Kumcu <68655812+devmdeniz@users.noreply.github.com>
2024-09-17 19:27:59 +0300

    Update update-readme.js

--- update-readme.js
+++ update-readme.js
@@ -1,8 +1,10 @@
 const fs = require('fs');
 
+// Dosyaları oku
 const projectsFile = fs.readFileSync('Projects.md', 'utf8');
 const readmeFile = fs.readFileSync('README.md', 'utf8');
 
+// #tables başlığındaki kısmı ayıkla
 const tablesSectionRegex = /<!--START_SECTION:ProjectsList-->([\s\S]*?)<!--END_SECTION:ProjectsList-->/i;
 const projectsTableMatch = projectsFile.match(tablesSectionRegex);
 if (!projectsTableMatch) {
@@ -12,20 +14,22 @@ if (!projectsTableMatch) {
 
 const projectsTable = projectsTableMatch[1];
 
+
 const rows = projectsTable
   .trim()
   .split('\n')
   .slice(2)
   .map(row => row.split('|').map(cell => cell.trim()))
-  .filter(row => row.length > 1)
+  .filter(row => row.length > 1 && row[3] !== '')
   .sort((a, b) => parseInt(b[3]) - parseInt(a[3]))
   .slice(0, 5);
-console.log(rows)
-// Yeni tabloyu oluştur
+
+
 const updatedTable = ['| Project name | Description | Main Language or Technology | Commit Number | STATE | STATUS |', 
                       '| :--- | :----: | ---: | ---: | ---: | ---: |']
-  .concat(rows.map(row => `| ${row.join(' | ')} |`))
+  .concat(rows.map(row => `| ${row.slice(1).join(' | ')} |`)) 
   .join('\n');
+
 
 const updatedReadme = readmeFile.replace(tablesSectionRegex, `<!--START_SECTION:ProjectsList-->\n${updatedTable}\n<!--END_SECTION:ProjectsList-->`);
 
